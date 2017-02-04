afe92c9322f1387b890594702a67e045cb4b50be
Ronan COLLEAUX <rcolleau@e3r9p4.42.fr>
2017-02-04 18:01:36 +0100

    new str_n_ft

--- libft/ft_strndup.c
+++ libft/ft_strndup.c
@@ -1,25 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnjoin.c                                      :+:      :+:    :+:   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rcolleau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/02/04 17:46:10 by rcolleau          #+#    #+#             */
-/*   Updated: 2017/02/04 17:46:15 by rcolleau         ###   ########.fr       */
+/*   Created: 2017/02/04 17:51:11 by rcolleau          #+#    #+#             */
+/*   Updated: 2017/02/04 17:54:01 by rcolleau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnjoin(char const *s1, char const *s2, size_t len)
+char	*ft_strndup(const char *s1, size_t len)
 {
-	char	*new;
+	char	*s2;
+	size_t	i;
 
-	if (!s1 || !s2 || !(new = ft_strnew(ft_strlen(s1) + (len + 1))))
-		return (0);
-	ft_strcat(new, s1);
-	ft_strncat(new, s2, len);
-	new[ft_strlen(new)] = '\0';
-	return (new);
+	i = 0;
+	if ((s2 = (char *)malloc(sizeof(char) * len)) == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
 }
--- libft/ft_strnjoin_free.c
+++ libft/ft_strnjoin_free.c
@@ -1,25 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnjoin.c                                      :+:      :+:    :+:   */
+/*   ft_strnjoin_free.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rcolleau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/02/04 17:46:10 by rcolleau          #+#    #+#             */
-/*   Updated: 2017/02/04 17:46:15 by rcolleau         ###   ########.fr       */
+/*   Created: 2017/02/04 17:55:00 by rcolleau          #+#    #+#             */
+/*   Updated: 2017/02/04 17:56:50 by rcolleau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnjoin(char const *s1, char const *s2, size_t len)
+char	*ft_strnjoin_free(char *s1, char const *s2, size_t len)
 {
-	char	*new;
+	char	*s;
+	size_t	l;
 
-	if (!s1 || !s2 || !(new = ft_strnew(ft_strlen(s1) + (len + 1))))
-		return (0);
-	ft_strcat(new, s1);
-	ft_strncat(new, s2, len);
-	new[ft_strlen(new)] = '\0';
-	return (new);
+	l = (ft_strlen(s1) + len);
+	if (!(s = (char *)malloc(sizeof(char) * (l + 1))))
+		return (NULL);
+	ft_strcpy(s, s1);
+	ft_strncat(s, s2, len);
+	ft_strdel(&s1);
+	return (s);
 }
