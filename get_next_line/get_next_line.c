377f130d970bd144d1f4db63a031ebd0733f5732
Ronan COLLEAUX <rcolleau@e2r11p4.42.fr>
2017-02-13 17:00:45 +0100

    gnl with list

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -5,117 +5,73 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rcolleau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/02/04 16:47:13 by rcolleau          #+#    #+#             */
-/*   Updated: 2017/02/04 16:47:28 by rcolleau         ###   ########.fr       */
+/*   Created: 2017/02/13 12:46:23 by rcolleau          #+#    #+#             */
+/*   Updated: 2017/02/13 12:46:26 by rcolleau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "libft/libft.h"
 
-char	**ft_alloc(char **tbl, int i)
+int		lst_find_link(const t_list *lst, int mys)
 {
-	if (!tbl)
+	t_list	*find_me;
+
+	if (!lst)
+		return (NULL);
+	find_me = (t_list *)lst;
+	while (find_me && (int)find_me->content_size != mys)
+		find_me = find_me->next;
+	return (find_me);
+}
+
+int		for_read(const int fd, char **content, char **buf, int ret)
+{
+	int		i;
+	char	*tmp;
+
+	while (ret = read(fd, *buf, BUFF_SIZE))
 	{
-		tbl = (char **)malloc(sizeof(char *) * OPEN_MAX);
-		tbl[i] = ft_memalloc(BUFF_SIZE + 1);
+		if (ret == -1)
+			return (-1)
+		*buf[ret] = '\0'
+		i = ft_intchr(*buf, '\n');
+		*buf[i] = '\0';
+		tmp = ft_strjoin_free(*content, *buf);
+		*content = tmp;
 	}
-	if (!tbl[i])
-		tbl[i] = ft_memalloc(BUFF_SIZE + 1);
-	return (tbl);
+	return (ret);
+}
+
+int		to_fill()
+{
+	return (1);
 }
 
 int		get_next_line(const int fd, char **line)
 {
-	static char		**stock;
-	ssize_t			i;
-	ssize_t			ret;
+	static t_list	*stock;
+	t_list			*new;
+	char			*buf;
+	int				ret;
 
-	i = 0;
-	if (line)
-		ft_strclr(*line);
-	if (fd < 0 )
+	ret = 0;
+	if (!fd || fd <= 0)
 		return (-1);
-	if (!stock || !stock[fd])
-		stock = ft_alloc(stock, fd);
-	*line = ft_memalloc(ft_strlen(stock[fd]) + BUFF_SIZE);
-	if (ft_strlen(stock[fd]))
+	if (line && *line)
+		ft_strdel(line);
+	if (!(new = find_link(stock, fd)))
 	{
-		if ((i = ft_intchr(stock[fd], '\n')) < (ssize_t)(ft_strlen(stock[fd])))
-		{
-			ft_strncpy(*line, stock[fd], (i));
-			while (i)
-			{
-				stock[fd]++;
-				i--;
-				while (*stock[fd] == '\n')
-					stock[fd]++;
-			}
-			return (1);
-		}
-		else if ((read(fd, stock[fd], BUFF_SIZE) == 0) &&
-			(ft_strlen(stock[fd]) < BUFF_SIZE) && (stock[fd][ft_strlen(stock[fd])] != '\n'))
-		{
-			ft_strcpy(*line, stock[fd]);
-			ft_strclr(stock[fd]);
-			return (1);
-		}
-		else
-		{
-			ft_strcpy(*line, stock[fd]);
-			ft_strclr(stock[fd]);
-		}
+		if (!(buff = ft_lstnew("\0", fd)))
+			return (-1);
+		buff->content_size = fd;
+		ft_lstadd(&stock, buff);
 	}
-	while ((ret = read(fd, stock[fd], BUFF_SIZE)))
-	{
-		stock[fd][ret] = '\0';
-		if (stock[fd][ret - 1] == '\n')
-		{
-			stock[fd][ret - 1] = '\0';
-			if ((i = ft_intchr(stock[fd], '\n')) < (ret - 1))
-				ft_strncat(*line, stock[fd], i);
-			else
-				ft_strcat(*line, stock[fd]);
-			break ;
-		}
-		else if (*stock[fd] == '\n')
-		{
-			if (line)
-			{
-				i = 0;
-				while (*stock[fd] == '\n')
-					stock[fd]++;
-				break ;
-			}
-			else
-			{
-				while (*stock[fd] == '\n')
-					stock[fd]++;
-				if ((i = ft_intchr(stock[fd], '\n')) < (ssize_t)(ft_strlen(stock[fd])))
-					ft_strncat(*line, stock[fd], i);
-				else
-					ft_strcat(*line, stock[fd]);
-			}
-		}
-		else
-		{
-			if ((i = ft_intchr(stock[fd], '\n')) < (BUFF_SIZE - 1))
-			{
-				ft_strncat(*line, stock[fd], i);
-				break ;
-			}
-			else
-				ft_strcat(*line, stock[fd]);
-		}
-	}
-	while (i)
-	{
-		stock[fd]++;
-		i--;
-	}
-	while (*stock[fd] == '\n' && (ret != 0))
-		stock[fd]++;
-	if (ret <= 0)
-			return (ret = 0 ? 0 : -1);
+	buf = ft_memalloc(BUFF_SIZE + 1);
+	if ((ret = for_read(fd, (char **)&new->content, &buf, ret)) < 0)
+		return (-1);
+	if (ret == 0 && !new->content)
+		return (0);
+	if (!(to_fill()))
+		return (-1);
 	return (1);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rcolleau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/01/31 16:48:45 by rcolleau          #+#    #+#             */
-/*   Updated: 2017/01/31 16:48:47 by rcolleau         ###   ########.fr       */
+/*   Created: 2017/02/13 12:33:02 by rcolleau          #+#    #+#             */
+/*   Updated: 2017/02/13 12:33:04 by rcolleau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,8 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
-# include <limits.h>
 
-# define BUFF_SIZE 4
+# define BUFF_SIZE 5
 
 int		get_next_line(const int fd, char **line);
 
