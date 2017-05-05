2d2856ac2f7743c20cbf07bf6d446b079568095e
Ronan COLLEAUX <rcolleau@e2r9p1.42.fr>
2017-05-05 14:38:06 +0200

    parsing over

--- ft_ls/parse_arg.c
+++ ft_ls/parse_arg.c
@@ -1,80 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_arg.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcolleau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/05 14:34:29 by rcolleau          #+#    #+#             */
+/*   Updated: 2017/05/05 14:34:31 by rcolleau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft/libft.h"
 #include "ft_ls.h"
-#include <dirent.h>
-#include <stdio.h>
 
-int		fill_arg(char c, t_arg **arg)
+void	fill_arg(char c, t_arg **arg)
 {
 	t_arg	*tmp;
-	int		i;
 
-	i = 1;
 	tmp = *arg;
-	if (c == 'l')
-		tmp->l = 1;
-	else if (c == 'R')
-		tmp->upper_r = 1;
-	else if (c == 'r')
-		tmp->lower_r = 1;
-	else if (c == 'A')
-		tmp->upper_a = 1;
-	else if (c == 'a')
-		tmp->lower_a = 1;
-	else if (c == 't')
-		tmp->t = 1;
-	else if (c == 'G')
-		tmp->upper_g = 1;
-	else if (c == 'u')
-		tmp->u = 1;
-	else if (c == 'F')
-		tmp->upper_f = 1;
-	else
-		i = 0;
-	return (i);
+	tmp->l = (c == 'l' ? 1 : tmp->l);
+	tmp->upper_r = (c == 'R' ? 1 : tmp->upper_r);
+	tmp->lower_r = (c == 'r' ? 1 : tmp->lower_r);
+	tmp->upper_a = (c == 'A' ? 1 : tmp->upper_a);
+	tmp->lower_a = (c == 'a' ? 1 : tmp->lower_a);
+	tmp->lower_a = (c == 'f' ? 1 : tmp->lower_a);
+	tmp->t = (c == 't' ? 1 : tmp->t);
+	tmp->upper_g = (c == 'G' ? 1 : tmp->upper_g);
+	tmp->upper_u = (c == 'U' ? 1 : tmp->upper_u);
+	tmp->upper_u = (c == 'f' ? 1 : tmp->upper_u);
+	tmp->lower_u = (c == 'u' ? 1 : tmp->lower_u);
+	tmp->upper_f = (c == 'F' ? 1 : tmp->upper_f);
+	tmp->l = (c == '1' ? 0 : tmp->l);
 }
 
-char	*get_arg(char **tbl, t_arg **arg)
+char	*get_arg(char **tbl, t_arg *arg)
 {
-	char 	*ret;
-	t_arg	*tmp;
 	char	*list_arg;
 	int		i;
 	int		j;
 
 	i = 0;
-	j = 0;
-	ret = NULL;
+	j = -1;
 	while (tbl[i] && tbl[i][0] == '-')
+	{
+		if (!tbl[i][1])
+			return (NULL);
 		i++;
-	ft_putnbrendl(i);
+	}
 	list_arg = ft_memalloc(1);
-	while (j < i)
-	{
+	while (++j < i)
 		list_arg = ft_strjoin_free(list_arg, tbl[j] + 1);
-		j++;
-	}
-	ft_putendl(list_arg);
-	ret = "2";
-	if (ret != NULL)
+	j = -1;
+	while (list_arg[++j])
 	{
-		ft_putstr("ls: illegal option -- ");
-		ft_putendl(ret);
+		if (check_char_pres(list_arg[j], "AFGRUaflrtu1") == 0)
+			return (list_arg + j);
+		fill_arg(list_arg[j], &arg);
 	}
-	return (ret);
+	return (NULL);
 }
 
 int		main(int argc, char **argv)
 {
-	struct dirent	*rent;
-	DIR				*dir;
-	t_bla			*b;
-	t_bla			*t;
-	t_arg			*a;
+	t_arg			a;
 	char			*ill_arg;
 
+	if (argc)
+		a = (t_arg){0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 	if ((ill_arg = get_arg(argv + 1, &a)) != NULL)
 	{
-		return (error_func("usage: ls [-AFGRalrtu1] [file ...]"));
+		ft_putstr("ls: illegal option -- ");
+		ft_putendl(ft_strsub(ill_arg, 0, 1));
+		return (error_func("usage: ls [-AFGRUaflrtu1] [file ...]"));
 	}
 	return (0);
 }
